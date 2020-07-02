8e0a156e58288ba8d26ed7ca2dce0b96d349f1ef
Roberto Arruda <roberto0arruda@hotmail.com>
2020-07-02 10:51:42 -0400

    🔧 Iniciando vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import DefaultTemplate from './layouts/DefaultTemplate.vue'
 
+import router from './routes'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(DefaultTemplate),
+  router
 }).$mount('#app')
