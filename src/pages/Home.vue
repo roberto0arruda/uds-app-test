d2c62bb911b9050778cb1051cb0083eccac409e1
Roberto Arruda <roberto0arruda@hotmail.com>
2020-07-05 15:37:33 -0400

    💄 Home Atualizado

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,17 +16,15 @@
 
         <div class="col-lg-5 col-md-6 mb-4">
           <div class="card h-100">
-            <a href="#">
+            <router-link :to="{ name: 'catalogo' }">
               <img
                 class="card-img-top"
-                src="@/assets/imgs/acai.png"
+                src="@/assets/imgs/acai-frozen.jpeg"
                 alt
               />
-            </a>
+            </router-link>
             <div class="card-body">
-              <h4 class="card-title">
-                <p @click="dialogVisible = true">Açai na Tijela</p>
-              </h4>
+              <h4 class="card-title">Açai na Tijela</h4>
               <table class="table">
                 <thead>
                   <tr>
@@ -45,14 +43,11 @@
               </table>
               <p class="card-text">O melhor e mais delicioso açai do Amazonas</p>
             </div>
-            <div
-              class="card-footer card-footer-custom"
-              @click="dialogVisible = true"
-            >
-              <a href="#">
+            <div class="card-footer card-footer-custom">
+              <router-link :to="{ name: 'catalogo' }">
                 Monte o açai ideal
                 <i class="fas fa-cart-plus"></i>
-              </a>
+              </router-link>
             </div>
           </div>
         </div>
@@ -61,109 +56,6 @@
       <!-- /.row -->
     </div>
     <!-- /.col-lg-9 -->
-    <el-dialog
-      title="Escolher Açai"
-      :visible.sync="dialogVisible"
-      width="50%"
-      :before-close="handleClose"
-    >
-      <span>Realizar Pedido</span>
-      <el-form label-width="120px">
-        <el-form-item label="Tamanho">
-          <el-select
-            v-model="pedido.tamanho"
-            placeholder="Qual tamanho?"
-          >
-            <el-option
-              v-for="item in tamanhos"
-              :key="item.id"
-              :label="`${item.nome} (${item.medida})`"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="Sabor">
-          <el-select
-            v-model="pedido.sabor"
-            placeholder="Qual o sabor?"
-          >
-            <el-option
-              v-for="item in sabores"
-              :key="item.id"
-              :label="item.nome"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <span
-        slot="footer"
-        class="dialog-footer"
-      >
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button
-          type="primary"
-          @click="dialogVisible = false"
-        >Confirm</el-button>
-      </span>
-    </el-dialog>
   </div>
   <!-- /.row -->
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      dialogVisible: false,
-      pedido: {
-        tamanho: '',
-        sabor: ''
-      },
-      tamanhos: [
-        {
-          id: 1,
-          nome: 'pequeno',
-          medida: '300ml'
-        },
-        {
-          id: 2,
-          nome: 'médio',
-          medida: '500ml'
-        },
-        {
-          id: 3,
-          nome: 'grande',
-          medida: '700ml'
-        }
-      ],
-      sabores: [
-        {
-          id: 1,
-          nome: 'morango'
-        },
-        {
-          id: 2,
-          nome: 'banana'
-        },
-        {
-          id: 3,
-          nome: 'kiwi'
-        }
-      ]
-    }
-  },
-  methods: {
-    handleClose (done) {
-      this.$confirm('Are you sure to close this dialog?')
-        .then(_ => {
-          done()
-        })
-        .catch(_ => { })
-    }
-  }
-
-}
-</script>
