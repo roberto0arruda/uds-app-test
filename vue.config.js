d7a7829eada73a5370670622266a4870bfa8a5d4
Roberto Arruda <roberto0arruda@hotmail.com>
2020-07-15 08:27:12 -0400

    test: Editando vue.config.js para gh-pages build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/uds-app-test/'
-        : '/'
+        : '/',
+    chainWebpack: config => {
+        config.plugins
+            .delete('split-manifest')
+            .delete('inline-manifest')
+    }
 }
